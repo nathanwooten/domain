5ca3771e951aa3418ca4ced882e73f2afb987a51
Nathan Wooten <a8gid24akL@gmail.com>
2022-10-27 17:04:39 -0400

    Create SomeClass.php

---
+++ SomeType/SomeClass.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace SomeVendor\SomeType;
+
+class SomeClass
+{
+
+  public function __construct( $name, $phone, $email )
+  {
+
+    $this->name = $name;
+    $this->phone = $phone;
+    $this->email = $email;
+
+  }
+
+}
